443d05bb900fa46724cb7783aadb9ce903afc28b
Siarhei <siarhei.by@gmail.com>
2024-08-01 23:05:33 +0300

    feat: add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import logEvents from '@/middlewares/logEvents';
+import swaggerDocument from '@/common/swagger.json';
 
 const app = express();
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(logEvents);
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
